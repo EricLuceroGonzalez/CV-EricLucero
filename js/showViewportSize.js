1bad751f9db1b2ac9b7afc886098f30f0fe77d3f
EricLuceroGonzalez <35483789+EricLuceroGonzalez@users.noreply.github.com>
2018-01-22 00:20:27 -0500

    Add files via upload

--- js/showViewportSize.js
+++ js/showViewportSize.js
@@ -7,3 +7,11 @@ function displayWindowSize() {
     var myHeight = window.innerHeight;
     document.getElementById("dimensions").innerHTML = myWidth + "x" + myHeight;
 };
+
+window.addEventListener('load', function(){ // on page load
+ 
+    document.getElementById("dimensions").addEventListener('touchstart', function(e){
+        alert(e.changedTouches[0].pageX) // alert pageX coordinate of touch point
+    }, false)
+ 
+}, false)
