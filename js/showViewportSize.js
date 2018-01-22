b3fbd4735cdfe0754fa1dbb6e2ea7792333007dd
EricLuceroGonzalez <35483789+EricLuceroGonzalez@users.noreply.github.com>
2018-01-22 00:47:24 -0500

    Add files via upload

--- js/showViewportSize.js
+++ js/showViewportSize.js
@@ -19,6 +19,7 @@ window.addEventListener('load', function(){
         var touchobj = e.changedTouches[0] // reference first touch point (ie: first finger)
         startx = parseInt(touchobj.clientX) // get x position of touch point relative to left edge of browser
         statusdiv.innerHTML = 'Status: touchstart<br> ClientX: ' + startx + 'px'
+        statusdiv.style.backgroundColor = 'black';
         e.preventDefault()
     }, false)
  
@@ -26,12 +27,14 @@ window.addEventListener('load', function(){
         var touchobj = e.changedTouches[0] // reference first touch point for this event
         var dist = parseInt(touchobj.clientX) - startx
         statusdiv.innerHTML = 'Status: touchmove<br> Horizontal distance traveled: ' + dist + 'px'
+        statusdiv.style.backgroundColor = 'blue';
         e.preventDefault()
     }, false)
  
     box1.addEventListener('touchend', function(e){
         var touchobj = e.changedTouches[0] // reference first touch point for this event
         statusdiv.innerHTML = 'Status: touchend<br> Resting x coordinate: ' + touchobj.clientX + 'px'
+        statusdiv.style.backgroundColor = 'yellow';
         e.preventDefault()
     }, false)
  
