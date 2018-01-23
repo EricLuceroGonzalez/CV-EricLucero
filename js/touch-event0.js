7de3aa76c2770d1289b9e1ee0bc0d83586e11030
EricLuceroGonzalez <35483789+EricLuceroGonzalez@users.noreply.github.com>
2018-01-23 14:29:29 -0500

    Add files via upload

--- js/touch-event0.js
+++ js/touch-event0.js
@@ -19,3 +19,13 @@ window.addEventListener('load', function(){
     }, false)
  
 }, false)
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
