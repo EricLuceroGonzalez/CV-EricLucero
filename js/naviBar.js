2578acfbb6d5743b9dfc943f0fda65426eb771e7
EricLuceroGonzalez <35483789+EricLuceroGonzalez@users.noreply.github.com>
2018-01-25 17:39:09 -0500

    Add files via upload

--- js/naviBar.js
+++ js/naviBar.js
@@ -1,11 +1,14 @@
 document.getElementById("clickMenu").addEventListener("click", openNav);
 document.getElementById("closeMenu").addEventListener("click", closeNav);
+
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
     document.getElementById("navWrapid").classList.add('allNB-clck');
     document.getElementById("topSpan").classList.add('topNB-clck');
     document.getElementById("midSpan").classList.add('middNB-clck');
     document.getElementById("bottSpan").classList.add('bottNB-clck');
+    document.getElementById("bara1").classList.add('claseAnim1');
+    document.getElementById("bara2").classList.add('claseAnim2');
 }
 
 function closeNav() {
@@ -14,4 +17,7 @@ function closeNav() {
     document.getElementById("topSpan").classList.remove('topNB-clck');
     document.getElementById("midSpan").classList.remove('middNB-clck');
     document.getElementById("bottSpan").classList.remove('bottNB-clck');
+    document.getElementById("bara1").classList.remove('claseAnim1');
+    document.getElementById("bara2").classList.remove('claseAnim2');
+
 }
