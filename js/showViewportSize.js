90e4ca03771e7f93942da402e17b4462ddfede21
EricLuceroGonzalez <35483789+EricLuceroGonzalez@users.noreply.github.com>
2018-02-07 00:39:41 -0500

    Add files via upload

--- js/showViewportSize.js
+++ js/showViewportSize.js
@@ -3,7 +3,9 @@ window.onscroll = function() {
 };
 
 function scrollSurprise() {
-  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+  var a = document.body.scrollTop;
+  var b= document.documentElement.scrollTop;
+  if (a > 550 ||  b > 550) {
     document.getElementById('divChatMe').style.opacity = "1";
     document.getElementById('dimensions').style.opacity = "1";
     document.getElementById('underConstr').style.opacity = "1";
@@ -13,8 +15,8 @@ function scrollSurprise() {
     document.getElementById('divChatMe').classList.remove("floatDiv-Hide");
     document.getElementById('dimensions').classList.remove("floatDiv-Hide");
     document.getElementById('underConstr').classList.remove("floatDiv-Hide");
-    document.getElementById('svg-frame2').classList.add("svg-frame2");
-    document.getElementById('svg-frame3').classList.add("svg-frame3");
+    document.getElementById('svg-IDframe2').classList.add("svg-frame2");
+    document.getElementById('svg-IDframe3').classList.add("svg-frame3");
   } else {
     document.getElementById('divChatMe').classList.remove("floatDivShow");
     document.getElementById('dimensions').classList.remove("floatDivShow");
@@ -22,15 +24,12 @@ function scrollSurprise() {
     document.getElementById('divChatMe').classList.add("floatDiv-Hide");
     document.getElementById('dimensions').classList.add("floatDiv-Hide");
     document.getElementById('underConstr').classList.add("floatDiv-Hide");
-    document.getElementById('svg-frame2').classList.remove("svg-frame2");
-    document.getElementById('svg-frame3').classList.remove("svg-frame3");    
-    // document.getElementById('divChatMe').style.opacity = "0";
-    // document.getElementById('dimensions').style.opacity = "0";
-    // document.getElementById('underConstr').style.opacity = "0";
+    document.getElementById('svg-IDframe2').classList.remove("svg-frame2");
+    document.getElementById('svg-IDframe3').classList.remove("svg-frame3");
   }
-  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-    document.getElementById('downArrow').classList.add("floatDiv-Hide");
-  } else {
-    document.getElementById('downArrow').classList.remove("floatDiv-Hide");
-  }
+  // if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+  //   document.getElementById('downArrow').classList.add("floatDiv-Hide");
+  // } else {
+  //   document.getElementById('downArrow').classList.remove("floatDiv-Hide");
+  // }
 }
